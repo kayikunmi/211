cba8aeedd0609506d8cbf1f93ba941ce91a2ba73
Kayikunmi <kayikunmiba@gmail.com>
2022-09-08 21:53:12 -0400

    swapppp

--- HW0/LinkedList.java
+++ HW0/LinkedList.java
@@ -24,6 +24,7 @@ public class LinkedList {
     }
   }
 
+
   public String traverse(){
     String retStr = "";
     Node current = head;
@@ -124,75 +125,148 @@ public class LinkedList {
       while (temp != null) {
         Node nodep = head;
         for (int z = 0; z < p; z++) {
-          nodep = nodep.next; 
+          nodep = nodep.next;
         }
-        Node nodepPrev = head;
-        for (int z = 0; z < p-1; z++) {
-          nodepPrev = nodepPrev.next; 
-        }
+        Node copy1 = nodep;
+        copy1.next = nodep.next;
+        copy1.prev = nodep.prev;
         Node nodeq = head;
         for (int y = 0; y < q; y++) {
           nodeq = nodeq.next;
         }
+
+        Node copy2 = nodeq;
+        copy2.next = copy1.next;
+        copy2.prev = copy1.prev;
+        copy2.prev.next = copy1;
+        copy2.next.prev = copy1;
+        copy1.next = nodeq.next;
+        copy1.prev = nodeq.prev;
+        copy1.prev.next  = copy1;
+        copy1.next.prev = copy1;
+        
+
+        
+
+
+
+        
+
+
+
+
+
+
+
+// Node nodepPrev = head;
+        // for (int z = 0; z < p-1; z++) {
+        //   nodepPrev = nodepPrev.next; 
+        // }
+
+
+
+        
+        /*save the first one as a temp node,
+        keep iterating till the second node, save that as a temp
+        take first temp's next/ prev and make them the second's next/prev
+        node 2 will have node one's next/prev.
+        set OG node  two's as the first temp's n/p
+
+        */
+     
+        
+        //starmye -> smartye
+
+        //iterate through list, copy node 1 node 2
+        //iterate through again to node one
+        // node2copy.next =  node1.next, node2copy.prev = node1.prev, node1.prev.next = node2copy
+        //node1.next.prev = node2copy 
+
+        
+
+
+        // nodep.prev.next = nodeq; 
+        // nodep.next.prev = nodeq;
+
+        // Node pTemp = nodep;
+        // Node qTemp = nodeq;
+        // Node pTempPrev = nodep.prev;
+        // Node pTempNext = nodep.next;
+        // Node qTempPrev = nodeq.prev;
+        // Node qTempNext = nodeq.next;
+
+
+
+        // nodep = nodeq;
+        // nodeq = pTemp;
+
+
+        
+
         // String data = nodep.data;
         // nodep.data = nodeq.data;
         // nodeq.data = data;
-        
-
-        Node pPrev = nodepPrev; //s
-        //System.out.println(pPrev.data);
-        //System.out.println(nodep.data);//t
-        Node pNext = nodep.next;//a
-        //System.out.println(pNext.data);
-        Node pNextNext = nodep.next.next;
-        //System.out.println(pNextNext.data);
-        Node qPrev = nodeq.prev; //m
-        //System.out.println(qPrev.data);
-        //System.out.println(nodeq.data);//y
-        ////
-        Node qNext = nodeq.next; //e
-      //System.out.println(head.data);
-Node tp = nodep;
-nodeq.next = nodep.next;
-      head.next = nodeq;
-      //nodep.prev = head;
-      //nodep.next = 
-
-        
-        
-
-        
-
-
-
-        //Node tempPrev = qPrev;
-        // Node pPrevNext = null; 
-        // pPrevNext = pPrev.next;//y
-        // pPrev.next = nodeq;
-        
-        
-        // qPrev = pPrev;
-
-        
-
-        
-      
-
-        // nodeq.prev.next = nodep;
-        // nodeq.next.prev = nodep;
-        // nodepPrev.next = nodeq;
-        // nodep.next.prev = nodeq;
-
-      
-        // System.out.println("prev: " + qPrev.data);
-      
-        
-
         return true;
       }
     }
     return false;  
   }
+        
+
+//         Node pPrev = nodepPrev; //s
+//         //System.out.println(pPrev.data);
+//         //System.out.println(nodep.data);//t
+//         Node pNext = nodep.next;//a
+//         //System.out.println(pNext.data);
+//         Node pNextNext = nodep.next.next;
+//         //System.out.println(pNextNext.data);
+//         Node qPrev = nodeq.prev; //m
+//         //System.out.println(qPrev.data);
+//         //System.out.println(nodeq.data);//y
+//         ////
+//         Node qNext = nodeq.next; //e
+//       //System.out.println(head.data);
+// Node tp = nodep;
+// nodeq.next = nodep.next;
+//       //head.next = nodeq;
+//       //nodep.prev = head;
+//       //nodep.next = 
+
+        
+        
+
+        
+
+
+
+//         //Node tempPrev = qPrev;
+//         // Node pPrevNext = null; 
+//         // pPrevNext = pPrev.next;//y
+//         // pPrev.next = nodeq;
+        
+        
+//         // qPrev = pPrev;
+
+        
+
+        
+      
+
+//         // nodeq.prev.next = nodep;
+//         // nodeq.next.prev = nodep;
+//         // nodepPrev.next = nodeq;
+//         // nodep.next.prev = nodeq;
+
+      
+//         // System.out.println("prev: " + qPrev.data);
+      
+        
+
+  //       return true;
+  //     }
+  //   }
+  //   return false;  
+  // }
 
   public void reverse() {
     Node prev = null;
@@ -257,6 +331,66 @@ nodeq.next = nodep.next;
 
   //           }}}
 
-}  
+
+  // public void swapNodes(int x, int y) 
+  //   { 
+  //     Node temp = head;
+  //   if (this.head != null && this.head.data != null) {
+  //     while (temp != null) {
+  //       Node nodep = head;
+  //       for (int z = 0; z < x; z++) {
+  //         nodep = nodep.next; 
+  //       }
+  //       Node nodepPrev = head;
+  //       for (int z = 0; z < y-1; z++) {
+  //         nodepPrev = nodepPrev.next; 
+  //       }
+  //       Node nodeq = head;
+  //       for (int b = 0; b < y; b++) {
+  //         nodeq = nodeq.next;
+  //       }
+  //       // Nothing to do if x and y are same 
+  //       if (x == y) return; 
+
+  //       // Search for x (keep track of prevX and CurrX) 
+  //       Node prevX = null, currX = head; 
+  //       while (currX != null && currX.data != nodep.data) 
+  //       { 
+  //           prevX = currX; 
+  //           currX = currX.next; 
+  //       } 
+
+  //       // Search for y (keep track of prevY and currY) 
+  //       Node prevY = null, currY = head; 
+  //       // while (currY != null && currY.data != nodeq.data) 
+  //       // { 
+  //       //     prevY = currY; 
+  //       //     currY = currY.next; 
+  //       // } 
+
+  //       // // If either x or y is not present, nothing to do 
+  //       // if (currX == null || currY == null) 
+  //       //     return; 
+
+  //       // // If x is not head of linked list 
+  //       // if (prevX != null) 
+  //       //     prevX.next = currY; 
+  //       // else //make y the new head 
+  //       //     head = currY; 
+
+  //       // // If y is not head of linked list 
+  //       // if (prevY != null) 
+  //       //     prevY.next = currX; 
+  //       // else // make x the new head 
+  //       //     head = currX; 
+
+  //       // Swap next pointers 
+  //       Node timp = currX.next; 
+  //       currX.next = currY.next; 
+  //       currY.next = timp; 
+
+  //   } 
+  // }}}
+  }
 
 
--- HW0/Test.java
+++ HW0/Test.java
@@ -14,25 +14,25 @@ public class Test {
         list.add(t);
         list.add(a);
         list.add(r);
-        list.add(k);
+        //list.add(k);
         list.add(y);//add(s) works
-        System.out.println("The traversered list: " + list.traverse()); //traverse works
-        System.out.println("Count of 's': " + list.count(s));//count(s) works
+        // System.out.println("The traversered list: " + list.traverse()); //traverse works
+        // System.out.println("Count of 's': " + list.count(s));//count(s) works
         list.add(m, q);
-        System.out.println("The list after adding at 3: " + list.traverse());//add(s,p) works
-        System.out.println("Count of 's': " + list.count(s));
-        System.out.println("Remove k: " + list.remove(k));
-        list.add("e");
-        System.out.println("list without k: " + list.traverse()); //traverse works
+        // System.out.println("The list after adding at 3: " + list.traverse());//add(s,p) works
+        // System.out.println("Count of 's': " + list.count(s));
+        // System.out.println("Remove k: " + list.remove(k));
+        // list.add("e");
+        System.out.println("list: " + list.traverse()); //traverse works
         //System.out.println("sawp: " + list.swap(p,q));
         list.swap(p,q);
         System.out.println("Swaped list: " + list.traverse()); //swap works
-        System.out.println("Remove all: " + list.removeAll(s)); //traverse works
+       // System.out.println("Remove all: " + list.removeAll(s)); //traverse works
         //System.out.println("Remove at q: " + list.remove(q)); //traverse works
         
-        list.reverse();
+        //list.reverse();
         
-        System.out.println("The reversed list: " + list.traverse());
+       // System.out.println("The reversed list: " + list.traverse());
     }
 
 } 

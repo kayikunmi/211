2a523c0e9722583fdbbd8eedf75d2cf164730969
kayikunmi <kayikunmiba@gmail.com>
2022-10-22 18:13:08 -0400

    starter files

---
+++ HW5/BinarySearchTree.java
@@ -0,0 +1,5 @@
+public class BinarySearchTree<K extends Comparable<K>,V>{
+   public static void main(String[] args) {
+        
+    }
+}
---
+++ HW5/Node.java
@@ -0,0 +1,10 @@
+public class Node { 
+    int key; 
+    Node left;
+    Node right; 
+
+    public Node(int data){ 
+        key = data; 
+        left = right = null; 
+    } 
+} 

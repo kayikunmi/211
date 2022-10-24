7c707a17729c924272359e7b7deac2eeed1bab69
kayikunmi <kayikunmiba@gmail.com>
2022-10-24 12:00:26 -0400

    hmmmmmm

--- HW5/BinarySearchTree.java
+++ HW5/BinarySearchTree.java
@@ -1,5 +1,13 @@
+import java.util.*;
+
 public class BinarySearchTree<K extends Comparable<K>,V>{
-   public static void main(String[] args) {
-        
-    }
+    Node<K,V> root; 
+   // Constructor for BST =>initial empty tree
+    BST_class(){ 
+        root = null; 
+    } 
+   public static void main(String[] args) {}
+   public void add(K k, V v) {}
+   public V remove(K key){}
+   public V lookup(K key) {}
 }

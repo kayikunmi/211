1d7092496bc791d3f7b9bb1465f8c441b41e6cf3
kayikunmi <kayikunmiba@gmail.com>
2022-10-24 14:23:04 -0400

    add works

--- HW5/BinarySearchTree.java
+++ HW5/BinarySearchTree.java
@@ -1,13 +1,78 @@
-import java.util.*;
+//import java.util.*;
+public class BinarySearchTree<K extends Comparable<K>, V> {
+    Node root;
+    BinarySearchTree() {
+        root = null;
+    }
+    
+    public void add(int key, int value) {
+        if (root == null) { //add at root
+            // Insert the new data, if root is null.
+            root = new Node(key, value);
+        }
+        else if ((int)(root.key) >= (int)(key)) {
+            // if current root data is greater than the new data then now process the left sub-tree
+            root.left = new Node(key, value);
+        } 
+        else {
+            // if current root data is less than the new data then now process the right sub-tree
+            root.right = new Node(key, value);
+        }
+        System.out.println("added " + key + " at " + value);
+    }
+      
+    public V delete(int key) {return null;}
+      
+    public int lookup(int key) {
+        // Key is equal to the key at root
+        if (root==null || root.key==key){
+            int lookupValue = root.value;
+            return lookupValue;
+        }
+        // Key is greater than root's key
+        if ((int)(root.key) < (int)(key)){
+            root = root.right;
+            return lookup(root.key);
+        }
+        // Key is smaller than root's key
+        else if((int)(root.key) < (int)(key)){
+            root = root.left;
+            return lookup(root.key);
+        }
+        else{
+            return -1;
+        }
+    }
+     
+    public void inOrderTraverse() {
+        inOrderTraverseRecursive(root);
+    }
+      // print the values in this BST in sorted order (to p)
 
-public class BinarySearchTree<K extends Comparable<K>,V>{
-    Node<K,V> root; 
-   // Constructor for BST =>initial empty tree
-    BST_class(){ 
-        root = null; 
-    } 
-   public static void main(String[] args) {}
-   public void add(K k, V v) {}
-   public V remove(K key){}
-   public V lookup(K key) {}
+    private void inOrderTraverseRecursive(Node root) {
+        if (root == null)
+            return;
+ 
+        /* first recur on left child */
+       inOrderTraverseRecursive(root.left);
+ 
+        /* then print the data of node */
+        System.out.print(root.key + " ");
+ 
+        /* now recur on right child */
+        inOrderTraverseRecursive(root.right);
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.add(5,5); //rn, i'm only adding at the root
+       bst.add(7,7);
+       bst.add(3,3);
+       bst.add(12,6);
+       bst.add(13,3);
+       bst.add(1,3);
+       System.out.println("bst root: " + bst.root.key);
+       bst.inOrderTraverse();
+
+    }
 }
--- HW5/Node.java
+++ HW5/Node.java
@@ -1,45 +1,33 @@
-// public class Node<K,V> { 
-//     K key; 
+// public class Node<K, V> {
+//     // *** fields ***
+//     K key;
 //     V value;
-//     Node left;
-//     Node right; 
-
-//     public Node(K data){ 
-//         key = data; 
-//         left = null;
-//         right = null; 
-//     } 
-
+//     Node<K, V> left, right;
+ 
+//     // *** constructor ***
 //     public Node(K key, V value) {
-// 		this.key = key;
-// 		this.value = value;
-// 	}
-
-// 	public String toString() {
-// 		String s = "\"" + key + ":" + value + "\"";
-// 		return s;
-// 	}
+//         this.key = key;
+//         this.value = value;
+//         this.left = left;
+//         this.right = right;
+//     }
+//     public V getValue() { 
+//         return value; 
+//     }
+//     public void setValue(V newV) { 
+//         value = newV; 
+//     }
+    
 // }
 
-public class Node<K, V> {
-    // *** fields ***
-    K key;
-    V value;
-    Node<K, V> left, right;
- 
-    // *** constructor ***
-    public Node(K key, V value,
-                         Node<K, V> left, Node<K, V> right) {
-        this.key = key;
-        this.value = value;
-        this.left = left;
-        this.right = right;
+class Node {
+    int key;
+    public int value;
+    Node left, right;
+   
+
+    public Node(int item, int value) {
+      key = item;
+      left = right = null;
     }
-    public V getValue() { 
-        return value; 
-    }
-    public void setValue(V newV) { 
-        value = newV; 
-    }
-    
-}
+  }

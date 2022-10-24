16b29ad4b597daef129cf37328964707c8c55824
kayikunmi <kayikunmiba@gmail.com>
2022-10-24 12:00:22 -0400

    hmmmm

--- HW5/Node.java
+++ HW5/Node.java
@@ -1,10 +1,45 @@
-public class Node { 
-    int key; 
-    Node left;
-    Node right; 
+// public class Node<K,V> { 
+//     K key; 
+//     V value;
+//     Node left;
+//     Node right; 
 
-    public Node(int data){ 
-        key = data; 
-        left = right = null; 
-    } 
-} 
+//     public Node(K data){ 
+//         key = data; 
+//         left = null;
+//         right = null; 
+//     } 
+
+//     public Node(K key, V value) {
+// 		this.key = key;
+// 		this.value = value;
+// 	}
+
+// 	public String toString() {
+// 		String s = "\"" + key + ":" + value + "\"";
+// 		return s;
+// 	}
+// }
+
+public class Node<K, V> {
+    // *** fields ***
+    K key;
+    V value;
+    Node<K, V> left, right;
+ 
+    // *** constructor ***
+    public Node(K key, V value,
+                         Node<K, V> left, Node<K, V> right) {
+        this.key = key;
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+    public V getValue() { 
+        return value; 
+    }
+    public void setValue(V newV) { 
+        value = newV; 
+    }
+    
+}

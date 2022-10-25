4d4a3c938c6d7ceb44a14f447deb479a9039c0ce
kayikunmi <kayikunmiba@gmail.com>
2022-10-25 16:30:55 -0400

    fix del

--- HW5/BinarySearchTree.java
+++ HW5/BinarySearchTree.java
@@ -29,54 +29,113 @@ public class BinarySearchTree<K extends Comparable<K>, V> {
         return root;
     }
       
-    public int delete(int key) {
-        Node curr = root;
-        return deleteRec(curr, key).value;
+    // public int delete(int key) {
+    //     Node curr = root;
+    //     return deleteRec(curr, key).value;
+    // }
+    // Node deleteRec(Node temp, int key) {
+    //     if(temp.key == key){
+    //         return temp;
+    //     }
+    //     //Return if the tree is empty
+    //     // else if (temp == null)
+    //       //return temp;
+    
+    //     // Find the node to be deleted
+    //     if (key < temp.key){
+    //         temp = deleteRec(temp.left, key);
+    //     }
+    //     else if (key > temp.key){
+    //         temp = deleteRec(temp.right, key);
+    //     }
+    //     else {
+    //       // If the node is with only one child or no child
+    //       if (temp.left == null){
+    //         return temp.right;
+    //       }
+    //       else if (temp.right == null){
+    //         return temp.left;
+    //     }
+    
+    //       // If the node has two children
+    //       // Place the inorder successor in position of the node to be deleted
+    //       temp.key = successor(temp.right);
+    
+    //       // Delete the inorder successor
+    //       temp.right = deleteRec(temp.right, temp.key);
+    //     }
+    
+    //     return temp;
+    // }
+    
+    // // Find the inorder successor
+    // int successor(Node temp) {
+    //     int minv = temp.key;
+    //     while (temp.left != null) {
+    //       minv = temp.left.key;
+    //       temp = temp.left;
+    //     }
+    //     return minv;
+    //   }
+
+
+    int delete(int key) { 
+        //Node del = deleteRec(root, key); 
+        //return del.value;
+        return deleteRec(root, key).value; 
     }
-    Node deleteRec(Node temp, int key) {
-        if(temp.key == key){
+  
+    /* A recursive function to 
+      delete an existing key in BST
+     */
+    Node deleteRec(Node temp, int key)
+    {
+        /* Base Case: If the tree is empty */
+        if (temp == null)
             return temp;
-        }
-        //Return if the tree is empty
-        // else if (temp == null)
-          //return temp;
-    
-        // Find the node to be deleted
-        if (key < temp.key){
-            temp = deleteRec(temp.left, key);
-        }
-        else if (key > temp.key){
-            temp = deleteRec(temp.right, key);
-        }
+  
+        /* Otherwise, recur down the tree */
+        if (key < temp.key)
+            temp.left = deleteRec(temp.left, key);
+        else if (key > temp.key)
+            temp.right = deleteRec(temp.right, key);
+  
+        // if key is same as root's 
+        // key, then This is the
+        // node to be deleted
         else {
-          // If the node is with only one child or no child
-          if (temp.left == null){
-            return temp.right;
-          }
-          else if (temp.right == null){
-            return temp.left;
+            // node with only one child or no child
+            if (temp.left == null){
+                temp = temp.right;
+                return temp;
+            }
+            else if (temp.right == null){
+                temp = temp.left;
+                return temp;
+            }
+  
+            // node with two children: Get the inorder
+            // successor (smallest in the right subtree)
+            temp.key = successor(temp.right);
+  
+            // Delete the inorder successor
+            temp.right = deleteRec(temp.right, temp.key);
+            temp.value = temp.right.value;
         }
-    
-          // If the node has two children
-          // Place the inorder successor in position of the node to be deleted
-          temp.key = successor(temp.right);
-    
-          // Delete the inorder successor
-          temp.right = deleteRec(temp.right, temp.key);
-        }
-    
+  
         return temp;
     }
-    
-    // Find the inorder successor
-    int successor(Node temp) {
-        int minv = temp.key;
-        while (temp.left != null) {
-          minv = temp.left.key;
-          temp = temp.left;
+  
+    int successor(Node root)
+    {
+        int minv = root.key;
+        while (root.left != null) 
+        {
+            minv = root.left.key;
+            root = root.left;
         }
         return minv;
-      }
+    }
 
     
 
@@ -124,16 +183,16 @@ public class BinarySearchTree<K extends Comparable<K>, V> {
         bst.add(7,7); //rn, i'm only adding at the temp
         bst.add(8,8);
         bst.add(3,3);
-        // bst.add(1,1);
-        // bst.add(5,5);
-        // bst.add(14, 14);
-        // bst.add(4,4);
+        bst.add(1,1);
+        bst.add(5,5);
+        bst.add(14, 14);
+        bst.add(4,4);
         
         System.out.println("bst root: " + bst.root.key);
         bst.inOrderTraverse();
         System.out.println();
         System.out.println("Lookup: " + bst.lookup(3));
-        System.out.println("Remove: " + bst.delete(3));
+        System.out.println("Remove: " + bst.delete(1));
         bst.inOrderTraverse();
         System.out.println();
         //bst.add(2,2);
